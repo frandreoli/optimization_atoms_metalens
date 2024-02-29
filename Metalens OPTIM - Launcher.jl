3ef47242902a75bee1ea5d5ffa48ac9fa603c121
frandreoli <fra.andreoli@hotmail.it>
2024-02-29 12:56:55 +0100

    Add Optim algorithms

--- Metalens OPTIM - Launcher.jl
+++ Metalens OPTIM - Launcher.jl
@@ -9,9 +9,8 @@ include("Metalens OPTIM - Core Evaluation.jl")
 #
 const z_fixed_option               =      ["YES" ; "NO"][1] #must be yes
 const phase_center_ring_option     =      ["YES" ; "NO"][1] #must be yes
-#const shift_to_match_option       =      ["YES" ; "NO"][2] #must be no
-#const phase_ring_integral_option  =      ["YES" ; "NO"][2] #must be no (?)
 const initial_guess_option         =      [true ; false][1]
+const use_blackboxoptim_option     =      [true ; false][2]
 #
 
 
@@ -40,25 +39,6 @@ const laser_detuning    =    0.0
 const w0_x              =    w0
 const w0_y              =    w0
 #
-#Defines the filename
-#=file_name="r"*string(r_lens/lambda0)[1:min(length(string(r_lens/lambda0)),3)]
-file_name*="_f"*string(focal_point/lambda0)[1:min(length(string(focal_point/lambda0)),3)]
-file_name*="_w"*string(w0/lambda0)[1:min(length(string(w0_x/lambda0)),3)]
-file_name*="_n"*string(nBulk)[1:min(3,length(string(nBulk)))]
-file_name*="_gPr"*string(gamma_prime)[1:min(4,length(string(gamma_prime)))]
-file_name*="_"*string(Dates.today())
-file_name="Metalens.OPTIM_"*file_name*args_checked[1]
-#
-#Initializes the folder
-println("\nTime: ",now(),"\nStarting evaluation of ", @__FILE__,"\n")
-println("Output file name: ",file_name,"\n\n")
-final_path_name="Data Out/"*file_name*"/"
-mkpath(final_path_name)
-#
-#Writing settings data file
-h5write_multiple(final_path_name*"inputs",        [("inputs",        [lambda0 w0 r_lens focal_point nBulk gamma_prime] )])
-#
-=#
 println("")
 println("#"^25)
 println("")
@@ -70,30 +50,42 @@ println("focal_point = ", focal_point)
 println("")
 println("#"^25)
 #
+
+
+################## OPTIMIZATION ###########################################################################################################
 #
-#Optimization set up
-#
-using BlackBoxOptim
-#
+#Optimization parameters
 thickness_range = (0.1,0.8)
-phase_range = (-pi,pi)
-buffer_range = (0,0.5)
+phase_range = (-3.141592653589793,3.141592653589793)
+buffer_range = (0.,0.5)
 initial_guess = [2.0/3, 1.0775,0.20048828125]#0.2]
 #
-chosen_solver = (
-    :adaptive_de_rand_1_bin,                             #1  Differential Evolution optimizer (metaheuristics)
-    :adaptive_de_rand_1_bin_radiuslimited,               #2  Suggested by developers
-    :resampling_memetic_search,                          #3  Memetic algorithm 
-    :resampling_inheritance_memetic_search,              #4  Memetic algorithm #VERY BAD
-    :simultaneous_perturbation_stochastic_approximation, #5  Stochastic Approximation algorithm #ERROR
-    :separable_nes,                                      #6  Natural Evolution Strategies
-    :xnes,                                               #7  Natural Evolution Strategies  #####
-    :dxnes,                                              #8  Natural Evolution Strategies  
-    :generating_set_search,                              #9  Generating set (direct) search #BEST UP TO NOW #####
-    :probabilistic_descent,                              #10 Generating set (direct) search #####
-    :borg_moea,                                          #11 http://borgmoea.org/ #Multi-objective optimization
-)[7]
+#Definition of the solver
+if use_blackboxoptim_option
+    #
+    using BlackBoxOptim
+    chosen_solver = (
+        :adaptive_de_rand_1_bin,                             #1  Differential Evolution optimizer (metaheuristics)
+        :adaptive_de_rand_1_bin_radiuslimited,               #2  Suggested by developers
+        :resampling_memetic_search,                          #3  Memetic algorithm 
+        :resampling_inheritance_memetic_search,              #4  Memetic algorithm #VERY BAD
+        :simultaneous_perturbation_stochastic_approximation, #5  Stochastic Approximation algorithm #ERROR
+        :separable_nes,                                      #6  Natural Evolution Strategies
+        :xnes,                                               #7  Natural Evolution Strategies  #####
+        :dxnes,                                              #8  Natural Evolution Strategies  
+        :generating_set_search,                              #9  Generating set (direct) search #BEST UP TO NOW #####
+        :probabilistic_descent,                              #10 Generating set (direct) search #####
+        :borg_moea,                                          #11 http://borgmoea.org/ #Multi-objective optimization
+    )[7]
+else
+    using Optim
+    chosen_solver = (
+        ParticleSwarm(; n_particles = 3),                    #1  Particle Swarm Optimization
+        SimulatedAnnealing(),                                #2  Simulated Annealing
+    )[1]
+end
 #
+#Printing data
 println("")
 println("The parameter ranges are: ")
 println("thickness_range = ", thickness_range)
@@ -105,22 +97,36 @@ if initial_guess_option
     println("- phase guess: "    , initial_guess[2])
     println("- buffer guess: "   , initial_guess[3])
 end
-println("Using the solver: "   , chosen_solver)
+println("Using the solver: "   , String(Symbol(chosen_solver)))
 println("")
 println("#"^25)
 println("\n\n")
 flush(stdout)
 #
-function objective_func(x)
-    # (disks_thickness, phase_shift, buffer_smooth) = Tuple(x)
+#Objective function
+function objective_func(x) 
+    #thickness, phase, buffer
     return 1.0-SM_main(x[1], x[2], x[3])
 end
 #
+#Initializing the optimization
 if initial_guess_option
-    #
     println("** STARTING the optimization with an initial guess **")
+    blackboxoptim_input = (objective_func, initial_guess)
+    initial_guess_Optim = initial_guess
+else
+    println("** STARTING the optimization with NO initial guess **")
+    blackboxoptim_input = (objective_func)
+    initial_guess_Optim = rand_range.([thickness_range, phase_range, buffer_range])
+end
+#
+function g_func!(G,x)
+    error("You shouldn't call the gradient function.")
+end
+#Optimization core
+if use_blackboxoptim_option
     #
-    optim_results = bboptimize(objective_func, initial_guess; 
+    optim_results = bboptimize(blackboxoptim_input...; 
         Method = chosen_solver, 
         SearchRange = [thickness_range, phase_range, buffer_range], 
         NumDimensions = 3,
@@ -132,26 +138,31 @@ if initial_guess_option
         TargetFitness = 0.0
     )
     #
+    x_results = best_candidate(optim_results)
+    #
 else
     #
-    println("** STARTING the optimization with NO initial guess **")
+    lower_range = (x->sort(collect(x))[1]).([thickness_range, phase_range, buffer_range])
+    upper_range = (x->sort(collect(x))[2]).([thickness_range, phase_range, buffer_range])
     #
-    optim_results = bboptimize(objective_func; 
-        Method = chosen_solver, 
-        SearchRange = [thickness_range, phase_range, buffer_range], 
-        NumDimensions = 3,
-        TraceMode = :compact, #:verbose, #:silent,
-        TraceInterval = 1,
-        PopulationSize = 50, #default is 50
-        CallbackFunction = x -> begin println("Efficiency = $(1-best_fitness(x)), $(best_candidate(x))"); flush(stdout) end,
-        CallbackInterval = 10,#1
-        TargetFitness = 0.0
+    optim_results = Optim.optimize(objective_func, g_func!, 
+        lower_range, 
+        upper_range, 
+        initial_guess_Optim,
+        Fminbox(chosen_solver), 
+        Optim.Options(
+            show_trace = true, 
+            show_every=1, 
+            callback = x -> begin flush(stdout) end,
+            extended_trace=true
+            )
     )
+    #
+    x_results = optim_results.minimizer
     #
 end
 #
-x_results=best_candidate(optim_results)
-#
+#Printing the final results
 println("#"^25)
 println("\n")        
 println("** Optimization result: " , best_fitness(optim_results))
@@ -159,3 +170,12 @@ println("** Optimal parameters: ")
 println("   1) disks_thickness     = "        , x_results[1])
 println("   2) phase_shift         = "        , x_results[2])
 println("   3) buffer_smooth       = "        , x_results[3])
+#
+
+
+################## FUNCTIONS #############################################################################################################
+#
+function rand_range(range)
+    sorted_range = sort(collect(range))
+    (sorted_range[2]-sorted_range[1])*rand()+sorted_range[1]
+end
--- Multiple_Launcher.sh
+++ Multiple_Launcher.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 echo "Launching Julia files"
-nameFile="COMPARE_SOLVERS_YES_GUESS"
+nameFile="PSO"
 nCores=32
 nThreads=32
 dateString=$(date +'%d-%m-%Y_%H.%M.%S')
+nameOut="Outputs/out_$nameFile""_n$nCores""_t$nThreads""_$dateString"
 
-nohup /usr/bin/time -v julia -p $nCores -t $nThreads "Metalens OPTIM - Launcher.jl" > "Outputs/out_$nameFile""_n$nCores""_t$nThreads""_$dateString.out" &
+nohup /usr/bin/time -v julia -p $nCores -t $nThreads "Metalens OPTIM - Launcher.jl" 2> "$nameOut""_ERROR.out" 1> "$nameOut"".out" &
 echo "Launching completed"
